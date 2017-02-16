645d8e11c86bab6892eb6dcef75d60196d62f0b7
zhaoqian <zhaoqian@weibangong.com>
2017-02-16 16:04:32 +0800

    asdfb

--- time_zone.py
+++ time_zone.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding=utf-8 -*-
-#add this aaaaaaaaaaisaaaawhat 
+#adsd this aaaaaaaaaaisaaaawhat 
 from datetime import datetime  as dt
 from datetime import timedelta
 from pytz import timezone
